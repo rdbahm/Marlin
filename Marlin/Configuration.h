e1f1f10e82f986b67027ed50d5ca6f109410db54
Ryan Bahm <rd.bahm@gmail.com>
2019-01-15 19:13:49 -0800

    Input skew measurements - as it turns out, I don't need correction.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1197,9 +1197,9 @@
 
 #if ENABLED(SKEW_CORRECTION)
   // Input all length measurements here:
-  #define XY_DIAG_AC 282.8427124746
-  #define XY_DIAG_BD 282.8427124746
-  #define XY_SIDE_AD 200
+  #define XY_DIAG_AC 140.92*2
+  #define XY_DIAG_BD 140.77*2
+  #define XY_SIDE_AD 41.04*2
 
   // Or, set the default skew factors directly here
   // to override the above measurements:
@@ -1207,13 +1207,13 @@
 
   //#define SKEW_CORRECTION_FOR_Z
   #if ENABLED(SKEW_CORRECTION_FOR_Z)
-    #define XZ_DIAG_AC 282.8427124746
-    #define XZ_DIAG_BD 282.8427124746
+    #define XZ_DIAG_AC 141*2
+    #define XZ_DIAG_BD 140.82*2
     #define YZ_DIAG_AC 282.8427124746
     #define YZ_DIAG_BD 282.8427124746
     #define YZ_SIDE_AD 200
-    #define XZ_SKEW_FACTOR 0.0
-    #define YZ_SKEW_FACTOR 0.0
+    //#define XZ_SKEW_FACTOR 0.0
+    //#define YZ_SKEW_FACTOR 0.0
   #endif
 
   // Enable this option for M852 to set skew at runtime
